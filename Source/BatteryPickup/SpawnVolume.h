e8c4b03b6657a530db06a1fe9ded7d74868af6d8
uu1477 <xuan.wu.shui@gmail.com>
2016-12-09 21:38:06 +0100

    save some change in the spawnvolume header file

--- Source/BatteryPickup/SpawnVolume.h
+++ Source/BatteryPickup/SpawnVolume.h
@@ -20,6 +20,17 @@ public:
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
+    //Returns the WhereToSpawn suboject 
+    FORCEINLINE class UBoxComponent* GetWhereToSpawn() const{return WhereToSpawn;}
+
+    //Find a random points within the BoxComponents
+    UFUNCTION(BlueprintPure, Category = "Spawning")
+    FVector GetRandomPointInVolume();
+
+    //This function toggles whether or not the spawn volume spawns pickups
+    UFUNCTION(BlueprintCallable, Category = "Spawning")
+    void SetSpawningActive(bool bShouldSpawn);
+
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Spawning", meta = (AllowPrivateAccess = "true"))
 	class UBoxComponent* WhereToSpawn;
