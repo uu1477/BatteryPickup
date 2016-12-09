95d43e84f1f4dc5cd347ec5c5b0ac7f95c639d64
uu1477 <xuan.wu.shui@gmail.com>
2016-12-09 22:15:38 +0100

    save some change

--- Source/BatteryPickup/SpawnVolume.cpp
+++ Source/BatteryPickup/SpawnVolume.cpp
@@ -1,14 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BatteryPickup.h"
+#include "BatteryCollector.h"
 #include "SpawnVolume.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Pickup.h"
 
 
 // Sets default values
 ASpawnVolume::ASpawnVolume()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Create the Box Component to represent the spawn volume
+	WhereToSpawn = CreateDefaultSubobject<UBoxComponent>(TEXT("WhereToSpawn"));
+	RootComponent = WhereToSpawn;
+
+	//Set the spawn delay range
+	SpawnDelayRangeLow = 1.0f;
+	SpawnDelayRangeHigh = 4.5f;
 
 }
 
@@ -16,6 +26,8 @@ ASpawnVolume::ASpawnVolume()
 void ASpawnVolume::BeginPlay()
 {
 	Super::BeginPlay();
+
+
 	
 }
 
@@ -26,3 +38,60 @@ void ASpawnVolume::Tick( float DeltaTime )
 
 }
 
+FVector ASpawnVolume::GetRandomPointInVolume()
+{
+	FVector SpawnOrigin = WhereToSpawn->Bounds.Origin;
+	FVector SpawnExtent = WhereToSpawn->Bounds.BoxExtent;
+
+	return UKismetMathLibrary::RandomPointInBoundingBox(SpawnOrigin, SpawnExtent);
+
+}
+
+void ASpawnVolume::SetSpawningActive(bool bShouldSpawn)
+{
+	if (bShouldSpawn)
+	{
+		// Set the timer on Spawn Pickup
+		SpawnDelay = FMath::FRandRange(SpawnDelayRangeLow, SpawnDelayRangeHigh);
+		GetWorldTimerManager().SetTimer(SpawnTimer, this, &ASpawnVolume::SpawnPickup, SpawnDelay, false);
+	}
+	else
+	{
+		// clear the timer on Spawn Pickup
+		GetWorldTimerManager().ClearTimer(SpawnTimer);
+	}
+}
+
+void ASpawnVolume::SpawnPickup()
+{
+	// If we have set something to spawn:
+	if (WhatToSpawn != NULL)
+	{
+		// Check for a valid World: 
+		UWorld* const World = GetWorld();
+		if (World)
+		{
+			// Set the spawn parameters
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = this;
+			SpawnParams.Instigator = Instigator;
+
+			// Get a random location to spawn at
+			FVector SpawnLocation = GetRandomPointInVolume();
+
+			// Get a random rotation for the spawned item
+			FRotator SpawnRotation;
+			SpawnRotation.Yaw = FMath::FRand() * 360.0f;
+			SpawnRotation.Pitch = FMath::FRand() * 360.0f;
+			SpawnRotation.Roll = FMath::FRand() * 360.0f;
+
+			// spawn the pickup
+			APickup* const SpawnedPickup = World->SpawnActor<APickup>(WhatToSpawn, SpawnLocation, SpawnRotation, SpawnParams);
+		
+			SpawnDelay = FMath::FRandRange(SpawnDelayRangeLow, SpawnDelayRangeHigh);
+			GetWorldTimerManager().SetTimer(SpawnTimer, this, &ASpawnVolume::SpawnPickup, SpawnDelay, false);
+		
+		}
+	}
+
+}
