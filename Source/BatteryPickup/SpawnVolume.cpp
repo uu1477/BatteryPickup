224a810b7de76617ec0ea8e4d405adb497155c22
uu1477 <xuan.wu.shui@gmail.com>
2016-12-09 22:21:21 +0100

    save some changes

--- Source/BatteryPickup/SpawnVolume.cpp
+++ Source/BatteryPickup/SpawnVolume.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BatteryCollector.h"
+#include "BatteryPickup.h"
 #include "SpawnVolume.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Pickup.h"
